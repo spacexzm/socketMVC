167d5b112bdd6a980440ec5ce4787a7d671a8942
spacexzm <spacexzm@163.com>
2019-08-06 11:23:48 +0800

    delete some useless annotation

--- server.py
+++ server.py
@@ -50,7 +50,6 @@ def run(host, port):
     """
     启动服务器
     """
-    # 初始化 socket 套路
     # 使用 with 可以保证程序中断的时候正确关闭 socket 释放占用的端口
     log('开始运行于', 'http://{}:{}'.format(host, port))
     with socket.socket() as s:
